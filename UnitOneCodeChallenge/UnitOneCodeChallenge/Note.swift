f55201bf58e55bb93e2b62145c4e56085916c971
SeanGilhuly <seangilhuly@hotmail.com>
2016-05-13 09:28:49 -0600

    Main Storyboard buildout
    Built out storyboard and files.

---
+++ UnitOneCodeChallenge/UnitOneCodeChallenge/Note.swift
@@ -0,0 +1,18 @@
+//
+//  Note.swift
+//  UnitOneCodeChallenge
+//
+//  Created by Sean Gilhuly on 5/13/16.
+//  Copyright © 2016 Sean Gilhuly. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    
+}
+
+
+
+
+
---
+++ UnitOneCodeChallenge/UnitOneCodeChallenge/NoteController.swift
@@ -0,0 +1,13 @@
+//
+//  NoteController.swift
+//  UnitOneCodeChallenge
+//
+//  Created by Sean Gilhuly on 5/13/16.
+//  Copyright © 2016 Sean Gilhuly. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    
+}
